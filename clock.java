29366e44831faa3f06bf0fec15e4456a42fb43ee
Victor Barajas <victorbarajas@Victors-MacBook-Pro.local>
2022-07-09 14:37:18 -0700

    building clock.java file

---
+++ clock.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class clock {
+    int hr = 1;
+    int min = 60;
+    int sec = 60;
+
+    if (sec == 60)public int getSec() {
+        return min;
+    }
+
+    public void setSec(int sec) {
+        this.sec = sec;
+    }
+
+    {
+        min = 1;
+    }
+
+    if (min == 60){
+        hr = 1;
+    }
+
+    if (hr == 24){
+        repeat;
+    }
+
+    public static void main(String[] args){
+
+    }
+
+    public int getHr() {
+        return hr;
+    }
+
+    public void setHr(int hr) {
+        this.hr = hr;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+}
